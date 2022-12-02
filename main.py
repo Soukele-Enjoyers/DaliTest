3aaf751dc4ddcb0bc6d11cf01fbeada5e4e79c81
nathan <nath.souvignet@gmail.com>
2022-12-02 03:20:57 +0100

    script pour compresser et prompt de génération de monstre ammélioré

--- main.py
+++ main.py
@@ -2,26 +2,70 @@
 import os
 import urllib.request
 
+import PIL.Image
 import rembg
 import openai
 
+import PIL.Image as PImg
+
 import urllib3.request
+
+folderPath = "C:/Users/crhon/Desktop/buffer";
+def getAverage(imageOpen:PIL.Image.Image, area, cellSize):
+    avg = (0,0,0)
+    for x in range(cellSize[0]):
+        for y in range(cellSize[1]):
+            avg2 = imageOpen.getpixel((area[0]+x, area[1] + y))
+            avg = (avg[0] + avg2[0], avg[1] + avg2[1], avg[2] + avg2[2])
+    totalPxl = cellSize[0]+cellSize[1]
+    return (int(avg[0]/totalPxl), int(avg[1]/totalPxl), int(avg[2]/totalPxl))
+def traitementImage(path, resolution, fileOutput):
+    image = PImg.open(path)
+    #image2 = open(folderPath + "/imagePixel.png", "wb")
+    proportions = (int(image.width/resolution[0]), int(image.height/resolution[1]))
+
+    imgPxl = PImg.new('RGBA', resolution, (0, 0, 0))
+    pxl = imgPxl.load()
+    for x in range(resolution[0]):
+        for y in range(resolution[1]):
+            pxl[x,y] = getAverage(image, (proportions[0]*x,proportions[1]*y), proportions)
+
+    imgPxl.save(fileOutput);
+
 
 openai.organization = "org-7GHrzPrQec6Q9QGVWpfDG3yC"
 openai.api_key = "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"
 openai.Model.list()
 
-nbIteration = 4
-folderPath = "C:/Users/crhon/Desktop/buffer";
 
-response = openai.Image.create(
-    prompt="3D representation of a AIDS virus with a stickman body, 8-bit style red palette",
-    n=nbIteration,
-    size="256x256"
-)
-for i in range(nbIteration):
-    fileInput = folderPath + "/image" + str(i) + ".png"
-    fileOutput = folderPath + "/image" + str(i) + str(nbIteration) + ".png"
-    url = (response['data'][i]['url'])
-    urllib.request.urlretrieve(url, fileInput)
-    open(fileOutput, 'wb').write(rembg.remove(open(fileInput, 'rb').read()))
+def createSprite():
+    nbIteration = 4
+
+    response = openai.Image.create(
+        prompt="AIDS virus as an 8-bit monster from a fantasy world, done using a red color palette",
+        n=nbIteration,
+        size="256x256"
+    )
+    for i in range(nbIteration):
+        fileInput = folderPath + "/image0-" + str(i) + ".png"
+        fileOutput = folderPath + "/image1-" + str(i) + ".png"
+        fileOutput2 = folderPath + "/image2-" + str(i) + ".png"
+
+        url = (response['data'][i]['url'])
+        urllib.request.urlretrieve(url, fileInput)
+        #traitementImage(fileInput, (32, 32), fileOutput)
+        open(fileOutput, 'wb').write(rembg.remove(open(fileInput, 'rb').read()))
+        #open(fileOutput, 'wb').write(rembg.remove(open(fileInput, 'rb').read()))
+
+
+def traitementTexte():
+    txt = open(folderPath + "/t.txt", 'r', encoding="utf-8")
+    buffer = txt.read()
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="summerize this:" +buffer
+    )
+    print(response['data'])
+
+createSprite()
